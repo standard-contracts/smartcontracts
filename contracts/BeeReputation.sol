576a5557a0ae68384e5256ac4d5886166c019408
rob <rob@thebeetoken.com>
2018-01-24 16:00:37 -0800

    Rep now accepts ether and bee + test updates

--- contracts/BeeReputation.sol
+++ contracts/BeeReputation.sol
@@ -1,11 +1,13 @@
 pragma solidity ^0.4.18;
 
+import 'zeppelin-solidity/contracts/token/ERC20.sol';
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
 
-contract Reputation is Ownable {
+contract BeeReputation is Ownable {
 
-
+    address public beeTokenAddress;
     struct PlatformStruct {
         address sharingPlatform;
         address platformAdmin;
@@ -16,8 +18,13 @@ contract Reputation is Ownable {
     // userId mapped to platform specific rep score
     mapping(bytes32 => mapping(uint8 => PlatformStruct)) public reputation;
 
-    function BeeReputation() public {
+    function BeeReputation(address _beeTokenAddress) public {
         //TODO: initilize with bootstrap reputation
+        beeTokenAddress = _beeTokenAddress;
+    }
+
+    // Accept eth as a donation
+    function () public payable {
     }
 
     function updateReputation(address platform) public onlyOwner {
@@ -30,6 +37,23 @@ contract Reputation is Ownable {
         // remove specified platform from reputation system
         revert();
     }
+
+    // User needs to call approve on token address before calling
+    function boostReputationWithBees(uint256 amount) public {
+        ERC20 tokenContract = ERC20(beeTokenAddress);
+        require(tokenContract.transferFrom(msg.sender, this, amount));
+    }
+
+    function ownerWithdrawBee() external onlyOwner {
+        ERC20 tokenContract = ERC20(beeTokenAddress);
+        uint256 tokenBalance = tokenContract.balanceOf(this);
+        require(tokenContract.transfer(owner, tokenBalance));
+    }
+
+    function ownerWithdrawEther() external onlyOwner {
+        owner.transfer(this.balance);
+    }
+
 
     function checkReputation(address platform, bytes32 userId) public pure returns(uint8) {
         // spit out the reputation score of an individual
--- test/BeeReputation.test.js
+++ test/BeeReputation.test.js
@@ -11,10 +11,33 @@ contract('BeeReputation Dispatch Test', function (accounts) {
 
     beforeEach(async function () {
         token = await BeeToken.new(user1, { from: owner });
-        reputation = await BeeReputation.new();
+        reputation = await BeeReputation.new(token.address, { from: owner });
     });
 
-    it("Should pass", async function() {
-        assert(true);
+    it("Should allow users to pay Bee to increase their reputation", async function() {
+        await token.enableTransfer();
+        let isEnabled = await token.transferEnabled();
+        assert(isEnabled, "transfers should be enabled");
+        await token.transfer(user2, 1000, { from: owner });
+        await token.transfer(user3, 1000, { from: owner });
+        let user2Balance = (await token.balanceOf(user2)).toNumber();
+        let user3Balance = (await token.balanceOf(user3)).toNumber();
+        assert.equal(user2Balance, 1000);
+        assert.equal(user3Balance, 1000);
+
+        await token.approve(reputation.address, 100, { from: user2 });
+
+        await reputation.boostReputationWithBees(100, { from: user2 });
+        let balanceCheck = (await token.balanceOf(reputation.address)).toNumber();
+        assert.equal(balanceCheck, 100)
+
     })
+    //TODO: should allow owner to withdraw ether
+    //TODO: should allow owner to withdraw Bee
+    //TODO: should allow owner to update new platforms
+    //TODO: should allow owner to add new admins
+    //TODO: should not allow non-admins/owner update scores
+    //TODO: should allow admin to update platform rep scores
+    //TODO should not admins to update other platforms
+    //
 });
